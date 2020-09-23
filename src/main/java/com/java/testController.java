d892f629aec936f0ec717014ba5bf8d6247a440c
Srivatsan1988 <58552415+Srivatsan1988@users.noreply.github.com>
2020-09-23 16:44:14 +0530

    hi

--- src/main/java/com/java/testController.java
+++ src/main/java/com/java/testController.java
@@ -1,7 +1,7 @@
 package com.java;
 
 
-//Trial From IDE
+//Trial From Git
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
